1dceb8525347926c17cb5a6b5d6b05b89a73abaa
Andocs <bondocan.markadrian.aratia@gmail.com>
2026-05-04 21:29:16 +0800

    Delete Main Code directory

--- Main Code/Main.java
+++
@@ -1,9 +0,0 @@
-package kyusiyusphere;
-
-public class Main {
-    
-    public static void main(String[] args) {
-        // Start the program from here
-        new startingScreen().setVisible(true);
-    }
-}
