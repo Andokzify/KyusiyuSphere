5ed5a642bebd4669e709dbbda2633618d4e4b48f
Andocs <bondocan.markadrian.aratia@gmail.com>
2026-05-07 06:25:34 +0800

    Delete Main Codes directory

--- Main Codes/Main.java
+++
@@ -1,8 +0,0 @@
-package kyusiyusphere;
-
-public class Main {
-    public static void main(String[] args) {
-        DatabaseHelper.initializeDatabase();  // Important!
-        new startingScreen().setVisible(true);
-    }
-}
